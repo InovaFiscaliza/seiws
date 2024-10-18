35068c6a83afd158ccc5881ff47869d93606de85
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2024-10-18 06:55:28 -0300

    ✨ feat: Adicionado teste `consultar_procedimento
    - Adiciona um novo conjunto de testes `TestConsultarProcedimento` para o método `consultar_procedimento` da classe `SeiClient`.
    - Parametriza os casos de teste para cobrir diferentes combinações de parâmetros de entrada

---
+++ tests/test_consultar_procedimento.py
@@ -0,0 +1,63 @@
+import os
+
+import pytest
+from seiws.client import SeiClient
+
+PROCESSOS_HM = [
+    "53500.000124/2024-04",  # Demanda Externa: Ministério Público Federal",
+    "53500.201128/2014-28",  # Demanda Externa: Órgãos Governamentais Federais
+]
+
+UNIDADES = ["SFI", "FISF", "FIGF"]
+
+FLAGS = ["S"]
+
+
+class TestConsultarProcedimento:
+    @pytest.fixture
+    def sei_client(self):
+        return SeiClient(
+            sigla_sistema="InovaFiscaliza",
+            chave_api=os.getenv("SEI_HM_API_KEY_BLOQUEIO"),
+        )
+
+    @pytest.mark.parametrize("sigla_unidade", UNIDADES)
+    @pytest.mark.parametrize("protocolo_procedimento", PROCESSOS_HM)
+    @pytest.mark.parametrize("sin_retornar_assuntos", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_interessados", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_observacoes", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_andamento_geracao", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_andamento_conclusao", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_ultimo_andamento", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_unidades_procedimento_aberto", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_procedimentos_relacionados", FLAGS)
+    @pytest.mark.parametrize("sin_retornar_procedimentos_anexados", FLAGS)
+    def test_consultar_procedimento_hm(
+        self,
+        sei_client,
+        sigla_unidade,
+        protocolo_procedimento,
+        sin_retornar_assuntos,
+        sin_retornar_interessados,
+        sin_retornar_observacoes,
+        sin_retornar_andamento_geracao,
+        sin_retornar_andamento_conclusao,
+        sin_retornar_ultimo_andamento,
+        sin_retornar_unidades_procedimento_aberto,
+        sin_retornar_procedimentos_relacionados,
+        sin_retornar_procedimentos_anexados,
+    ):
+        chamada = sei_client.consultar_procedimento(
+            sigla_unidade=sigla_unidade,
+            protocolo_procedimento=protocolo_procedimento,
+            sin_retornar_assuntos=sin_retornar_assuntos,
+            sin_retornar_interessados=sin_retornar_interessados,
+            sin_retornar_observacoes=sin_retornar_observacoes,
+            sin_retornar_andamento_geracao=sin_retornar_andamento_geracao,
+            sin_retornar_andamento_conclusao=sin_retornar_andamento_conclusao,
+            sin_retornar_ultimo_andamento=sin_retornar_ultimo_andamento,
+            sin_retornar_unidades_procedimento_aberto=sin_retornar_unidades_procedimento_aberto,
+            sin_retornar_procedimentos_relacionados=sin_retornar_procedimentos_relacionados,
+            sin_retornar_procedimentos_anexados=sin_retornar_procedimentos_anexados,
+        )
+        assert hasattr(chamada, "IdProcedimento")
