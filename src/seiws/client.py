bf48e394c1c209739cd58629293005d3884f84f9
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2024-10-18 04:51:15 -0300

    ✨ feat: Refactor `concluir_processo` and `reabrir_processo` methods
    A Chamada para a API estava incorreta no módulo "concluir_processo"

--- src/seiws/client.py
+++ src/seiws/client.py
@@ -132,7 +132,7 @@ class SeiClient:
             raise ValueError(f"Unidade inválida: {sigla_unidade}")
         id_unidade = self.unidades[sigla_unidade]["IdUnidade"]
         chamada = self._chamar_servico(
-            "reabrirProcesso",
+            "concluirProcesso",
             IdUnidade=id_unidade,
             ProtocoloProcedimento=protocolo_procedimento,
         )
@@ -180,7 +180,7 @@ class SeiClient:
         sin_retornar_observacoes: str = "N",  # S/N - sinalizador para retorno das observações das unidades
         sin_retornar_andamento_geracao: str = "N",  # S/N - sinalizador para retorno do andamento de geração
         sin_retornar_andamento_conclusao: str = "N",  # S/N - sinalizador para retorno do andamento de conclusão
-        sin_retornar_ultimo_andamento: str = "N",  #  S/N - sinalizador para retorno do último andamento
+        sin_retornar_ultimo_andamento: str = "S",  #  S/N - sinalizador para retorno do último andamento
         sin_retornar_unidades_procedimento_aberto: str = "N",  # S/N - sinalizador para retorno das unidades onde o processo está aberto
         sin_retornar_procedimentos_relacionados: str = "N",  # S/N - sinalizador para retorno dos processos relacionados
         sin_retornar_procedimentos_anexados: str = "N",  # S/N - sinalizador para retorno dos processos anexados
@@ -400,13 +400,21 @@ if __name__ == "__main__":
         sigla_sistema=sigla_sistema, chave_api=os.getenv("SEI_HM_API_KEY_BLOQUEIO")
     )
 
-    client.enviar_email(
-        "FISF",
-        "53500.000124/2024-04",
-        "rsilva@anatel.gov.br",
-        "eric@anatel.gov.br",
-        "mer.de.dirac@gmail.com",
-        assunto="Teste de Email via API",
-        mensagem="Este é um teste de email enviado via API do SEI",
-        documentos=["0206167"],
+    # client.enviar_email(
+    #     "FISF",
+    #     "53500.000124/2024-04",
+    #     "rsilva@anatel.gov.br",
+    #     "eric@anatel.gov.br",
+    #     "mer.de.dirac@gmail.com",
+    #     assunto="Teste de Email via API",
+    #     mensagem="Este é um teste de email enviado via API do SEI",
+    #     documentos=["0206167"],
+    # )
+
+    # client.concluir_processo("FISF", "53500.000124/2024-04")
+
+    # client.reabrir_processo("FISF", "53500.000124/2024-04")
+
+    client.consultar_procedimento(
+        "FISF", "53500.000124/2024-04", sin_retornar_ultimo_andamento="S"
     )
