f20843235b8a093c797615651a9a7e88345b89fe
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2024-11-04 23:06:15 -0300

    ✅ feat(client): Refactor `atribuir_processo` method
    - Moves the SOAP client instantiation and WSDL download logic to the `helpers` module
    - Updates the `atribuir_processo` method to use the new `instanciar_cliente_soap` and `download_wsdl` helpers
    - Parameterizes the test cases to use the new `sei_client` fixtures instead of a shared instance
    - Removes the hardcoded user IDs and instead iterates over the `usuarios` list in the `sei_client` instances

--- tests/test_atribuir_processo.py
+++ tests/test_atribuir_processo.py
@@ -2,41 +2,20 @@ import os
 
 import pytest
 from seiws.client import SeiClient
+from seiws.helpers import download_wsdl, instanciar_cliente_soap
+
 
 PROCESSOS_HM = [
-    "53500.000124/2024-04"  # Demanda Externa: Ministério Público Federal",
+    "53500.000124/2024-04",  # Demanda Externa: Ministério Público Federal",
     "53500.201128/2014-28",  # Demanda Externa: Órgãos Governamentais Federais
-]
-USUARIOS_SFI = [
-    "100000141",
-    "100003387",
-    "100000205",
-    "100000474",
-    "100000641",
-    "100003137",
-    "100000737",
-    "100001719",
-    "100003260",
-    "100001292",
-    "100003429",
-    "100001426",
+    "53500.059070/2019-18",  # Demanda Externa: Judiciário
+    "53569.200039/2014-70",  # Procuradoria: Cumprimento de Decisão Judicial
 ]
 
-sei_client = SeiClient(
-    sigla_sistema=os.getenv("SIGLA_SISTEMA"),
-    chave_api=os.getenv("SEI_HM_API_KEY_BLOQUEIO"),
-)
-
-USUARIOS_FISF = [
-    u["IdUsuario"] for u in sei_client.listar_usuarios(sigla_unidade="FISF")
-]
-
-USUARIOS_FIGF = [
-    u["IdUsuario"] for u in sei_client.listar_usuarios(sigla_unidade="FIGF")
-]
+CLIENTE_SOAP = instanciar_cliente_soap(download_wsdl("homologação"))
 
 
-class TestAtribuirProcesso:
+class TestAtribuirProcessoBloqueio:
     """
     Test the process assignment functionality for the SFI, FISF and FIGF unit.
 
@@ -44,52 +23,61 @@ class TestAtribuirProcesso:
     correctly assigns a process to a user in the unit.
 
     The test is parameterized with a list of process protocols (`PROCESSOS_HM`) and a list
-    of user IDs (`USUARIOS_*`). For each combination of process protocol and user ID,
+    of user IDs (client.usuarios). For each combination of process protocol and user ID,
     the test calls the `atribuir_processo` method and asserts that the call returns `True`,
     indicating that the process was successfully assigned.
     """
 
-    # @pytest.fixture
-    # def sei_client(self):
-    #     return SeiClient(
-    #         sigla_sistema=os.getenv("SIGLA_SISTEMA"),
-    #         chave_api=os.getenv("SEI_HM_API_KEY_BLOQUEIO"),
-    #     )
-
-    @pytest.mark.parametrize("protocolo_procedimento", PROCESSOS_HM)
-    @pytest.mark.parametrize("id_usuario", USUARIOS_SFI)
-    def test_atribuir_processo_hm_sfi(self, protocolo_procedimento, id_usuario):
-        assert sei_client.atribuir_processo(
+    @pytest.fixture
+    def sei_client_sfi(self):
+        return SeiClient(
+            cliente_soap=CLIENTE_SOAP,
+            sigla_sistema="InovaFiscaliza",
+            chave_api=os.environ["SEI_HM_API_KEY_BLOQUEIO"],
             sigla_unidade="SFI",
-            protocolo_procedimento=protocolo_procedimento,
-            id_usuario=id_usuario,
-            sin_reabrir="S",
+        )
+
+    @pytest.fixture
+    def sei_client_fisf(self):
+        return SeiClient(
+            cliente_soap=CLIENTE_SOAP,
+            sigla_sistema="InovaFiscaliza",
+            chave_api=os.environ["SEI_HM_API_KEY_BLOQUEIO"],
+            sigla_unidade="FISF",
+        )
+
+    @pytest.fixture
+    def sei_client_figf(self):
+        return SeiClient(
+            cliente_soap=CLIENTE_SOAP,
+            sigla_sistema="InovaFiscaliza",
+            chave_api=os.environ["SEI_HM_API_KEY_BLOQUEIO"],
+            sigla_unidade="FIGF",
         )
 
     @pytest.mark.parametrize("protocolo_procedimento", PROCESSOS_HM)
-    @pytest.mark.parametrize("id_usuario", USUARIOS_FISF)
-    def test_atribuir_processo_hm_fisf(
-        self,
-        id_usuario,
-        protocolo_procedimento,
-    ):
-        assert sei_client.atribuir_processo(
-            sigla_unidade="FISF",  # FISF
-            protocolo_procedimento=protocolo_procedimento,
-            id_usuario=id_usuario,
-            sin_reabrir="S",
-        )
+    def test_atribuir_processo_hm_sfi(self, sei_client_sfi, protocolo_procedimento):
+        for sigla_usuario in sei_client_sfi.usuarios:
+            assert sei_client_sfi.atribuir_processo(
+                protocolo_procedimento=protocolo_procedimento,
+                sigla_usuario=sigla_usuario,
+                sin_reabrir="S",
+            )
 
     @pytest.mark.parametrize("protocolo_procedimento", PROCESSOS_HM)
-    @pytest.mark.parametrize("id_usuario", USUARIOS_FIGF)
-    def test_atribuir_processo_hm_figf(
-        self,
-        id_usuario,
-        protocolo_procedimento,
-    ):
-        assert sei_client.atribuir_processo(
-            sigla_unidade="FIGF",  # FIGF
-            protocolo_procedimento=protocolo_procedimento,
-            id_usuario=id_usuario,
-            sin_reabrir="S",
-        )
+    def test_atribuir_processo_hm_fisf(self, sei_client_fisf, protocolo_procedimento):
+        for sigla_usuario in sei_client_fisf.usuarios:
+            assert sei_client_fisf.atribuir_processo(
+                protocolo_procedimento=protocolo_procedimento,
+                sigla_usuario=sigla_usuario,
+                sin_reabrir="S",
+            )
+
+    @pytest.mark.parametrize("protocolo_procedimento", PROCESSOS_HM)
+    def test_atribuir_processo_hm_figf(self, sei_client_figf, protocolo_procedimento):
+        for sigla_usuario in sei_client_figf.usuarios:
+            assert sei_client_figf.atribuir_processo(
+                protocolo_procedimento=protocolo_procedimento,
+                sigla_usuario=sigla_usuario,
+                sin_reabrir="S",
+            )
